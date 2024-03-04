502dd2d1375b69e96fe32a45aef9c270e00c0bbb
Foxglovery <foxglovery@gmail.com>
2024-03-04 10:35:47 -0600

    Add project files.

---
+++ ShoppingCart.Server/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace ShoppingCart.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShopController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ShopController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        [Route("ProductList")]
+        public string GetAllProducts()
+        {
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("").ToString());
+        }
+    }
+}
---
+++ ShoppingCart.Server/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoppingCart.Server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public decimal ActualPrice { get; set; }
+        public decimal DiscountedPrice { get; set; }
+    }
+}
